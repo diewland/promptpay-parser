e23af9547ec1b87bb98119043c77b19a6bf2cd21
diewland <diewland@gmail.com>
2022-03-28 21:23:02 +0700

    update parser logic

--- parser.py
+++ parser.py
@@ -9,30 +9,39 @@ def parse(txt):
 def validate_checksum(checksum):
     return True # TODO add logic
 
-def detect_nested_block(value):
-    return False # TODO add logic
-
-def rparse(txt, obj={}):
-
-    # end of text
-    if not txt:
-        return obj
-
-    # extract data from text
+def extract_block_data(txt):
     field_no = txt[:2]
     size = int(txt[2:4])
     value = txt[4:4+size]
-    new_txt = txt[4+size:]
+    next_txt = txt[4+size:]
+    return field_no, size, value, next_txt
 
-    # validate value
-    if size != len(value):
-        return None
-
-    # update object
-    if detect_nested_block(value):
-        obj[field_no] = rparse(value)
+def detect_block(txt):
+    idx = 0
+    while idx < len(txt):
+        new_txt = txt[idx:]
+        try:
+            _, size, value, _ = extract_block_data(new_txt)
+        except:
+            return False
+        if size <= 0 or size != len(value):
+            return False
+        idx += 4 + size
+    new_txt = txt[idx:]
+    if not new_txt:
+        return True
     else:
-        obj[field_no] = value
+        return False
 
-    # recursive
-    return rparse(new_txt, obj)
+def rparse(txt):
+    obj = {}
+    idx = 0
+    while idx < len(txt):
+        new_txt = txt[idx:]
+        field_no, size, value, _ = extract_block_data(new_txt)
+        if detect_block(value):
+            obj[field_no] = rparse(value)
+        else:
+            obj[field_no] = value
+        idx += 4 + size
+    return obj
